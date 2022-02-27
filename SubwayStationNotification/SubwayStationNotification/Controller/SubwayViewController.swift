3a4d23ac9df852eb05655ba17165222ceee9f8f0
Jangilkyu <jang19.dev@gmail.com>
2022-02-27 22:16:41 +0900

    Add: initial commit

---
+++ SubwayStationNotification/SubwayStationNotification/Controller/SubwayViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SubwayViewController.swift
+//  SubwayStationNotification
+//
+//  Created by 장일규 on 2022/02/27.
+//
+
+import UIKit
+
+class SubwayViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+    }
+}
