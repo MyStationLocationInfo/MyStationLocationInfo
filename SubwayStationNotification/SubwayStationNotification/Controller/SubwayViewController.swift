cb51b8d3e2e0ae280d43a19e23203c85a385e316
Jangilkyu <jang19.dev@gmail.com>
2022-03-01 14:45:19 +0900

    Add: MainView Label and Button component and Layout

--- SubwayStationNotification/SubwayStationNotification/Controller/SubwayViewController.swift
+++ SubwayStationNotification/SubwayStationNotification/Controller/SubwayViewController.swift
@@ -8,7 +8,69 @@
 import UIKit
 
 class SubwayViewController: UIViewController {
+    
+    var latitude: Double?
+    var longitude: Double?
+    
+    let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+            titleLabel.textAlignment = .center
+            titleLabel.text = "🚊터치하여 현재 station search하기"
+            titleLabel.textColor = .black
+            titleLabel.font = UIFont(name: "bold", size: 25)
+            titleLabel.adjustsFontSizeToFitWidth = true
+        
+            return titleLabel
+    }()
+    
+    let button: UIButton = {
+      let button = UIButton()
+        button.setTitle("현재 역 위치", for: .normal)
+        button.backgroundColor = .black
+        button.layer.cornerRadius = 16
+        button.addTarget(self, action: #selector(locationUpdateMarker), for: .touchUpInside)
+
+        return button
+    }()
+
     override func viewDidLoad() {
         view.backgroundColor = .white
+        setup()
     }
+    
+    func setup() {
+        addViews()
+        setConstraints()
+    }
+    
+    func addViews() {
+        view.addSubview(titleLabel)
+        view.addSubview(button)
+    }
+    
+    func setConstraints() {
+        titleLabelConstraints()
+        buttonConstraints()
+    }
+    
+    func titleLabelConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.topAnchor.constraint(equalTo: button.topAnchor, constant: -60).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    func buttonConstraints() {
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+    
+    @objc func locationUpdateMarker() {
+        debugPrint("button 이벤트")
+    }
+    
+    
 }
