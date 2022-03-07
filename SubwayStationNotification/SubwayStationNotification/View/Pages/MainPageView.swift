a96b733088fdeb35aebe040e1670976b80c483a3
Jangilkyu <jang19.dev@gmail.com>
2022-03-07 21:47:24 +0900

    Add: make View and Pages Directory and MainPageView

---
+++ SubwayStationNotification/SubwayStationNotification/View/Pages/MainPageView.swift
@@ -0,0 +1,20 @@
+//
+//  MainPageView.swift
+//  SubwayStationNotification
+//
+//  Created by 장일규 on 2022/03/07.
+//
+
+import UIKit
+
+class MainPageView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        return nil
+    }
+    
+}
