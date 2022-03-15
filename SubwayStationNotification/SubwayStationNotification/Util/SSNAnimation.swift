13b259a459a93aec8c57927d43da952ead899e04
Jangilkyu <jang19.dev@gmail.com>
2022-03-15 21:02:27 +0900

    Update show Animation

--- SubwayStationNotification/SubwayStationNotification/Util/SSNAnimation.swift
+++ SubwayStationNotification/SubwayStationNotification/Util/SSNAnimation.swift
@@ -5,8 +5,25 @@
 //  Created by 장일규 on 2022/03/15.
 //
 
-import Foundation
+import UIKit
 
 class SSNAnimation {
+    func show(
+        view: UIView,
+        duration: TimeInterval,
+         completion: (() -> Void)?
+    ) {
+        UIView.animate(
+            withDuration: duration,
+            delay: 0,
+            options: .curveEaseOut,
+            animations: {
+                view.alpha = 1
+            },
+            completion: {  _ in
+                completion?()
+            }
+        )
+    }
     
 }
