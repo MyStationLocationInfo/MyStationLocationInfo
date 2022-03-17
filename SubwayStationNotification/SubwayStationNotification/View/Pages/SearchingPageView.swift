ddef9611fb960633865e32eb9be3f902ff967e8b
Jangilkyu <jang19.dev@gmail.com>
2022-03-17 21:27:59 +0900

    Update: Add titleLabel and metroAnimation

--- SubwayStationNotification/SubwayStationNotification/View/Pages/SearchingPageView.swift
+++ SubwayStationNotification/SubwayStationNotification/View/Pages/SearchingPageView.swift
@@ -6,7 +6,39 @@
 //
 
 import UIKit
+import Lottie
 
 class SearchingPageView: UIView {
     
+    let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.textAlignment = .center
+        titleLabel.text = "현재 역 찾는중 ..."
+        titleLabel.textColor = .white
+        titleLabel.font = .boldSystemFont(ofSize: 30)
+        titleLabel.adjustsFontSizeToFitWidth = true
+        
+        return titleLabel
+    }()
+    
+    let metroLottie: AnimationView = {
+        let metroLottie:AnimationView = AnimationView(name: "metro")
+        metroLottie.play(
+            fromFrame: 0,
+            toFrame: 200,
+            loopMode: .loop,
+            completion: nil
+        )
+        metroLottie.animationSpeed = 2
+        
+        return metroLottie
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        return nil
+    }
 }
